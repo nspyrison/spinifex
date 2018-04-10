2d80dfc8811df7eb00182d2df22acb8c6de588a7
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-04-10 14:37:34 +1000

    check in try vignette on laptop.

--- _create_package.R
+++ _create_package.R
@@ -1,13 +1,13 @@
 ###create the package:
 #ns 5/04/2018.
 
-library(tourr)
+
 #devtools::create("spacemanip")
 devtools::document()
 #getwd()
 #setwd("C:/Users/nspy0001/Dropbox/_Main/R_dir/spacemanip/spacemanip")
 #setwd("C:/Users/nspy0001/Dropbox/_Main/R_dir/spacemanip")
-devtools::use_data(flea)
+#devtools::use_data(flea)
 devtools::use_vignette("introduction")
 
 #library(roxygen2)
@@ -19,5 +19,7 @@ packageVersion("devtools") #‘1.13.5’
 ?create_manip_space # works once the right .rd file has been made.
 
 
-devtools::document() #Invalid DESCRIPTION: Malformed maintainer field.
-options(devtools.desc.author="'First Last <first.last@example.com> [aut, cre]'") # didn't fix.
+devtools::load_all() #loads into session like library()
+
+devtools::install() #installs like install.package()
+library("spacemanip")
