5d0e25be4b354e84767794f5549182ec96674824
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-04-24 14:38:00 +1000

    check in before meeting. doc, vignette examples, projection.R work

--- R/projection.R
+++ R/projection.R
@@ -3,46 +3,52 @@
 #' Project [n, p] data onto a [p, 3] rotated manipulation space into the [n, 3] projection. Stores the data projection(s) in a data structure for plotting
 #'
 #' @param data [n, p] data to project, consisting of only numeric variables (for coercion into matrix)
-#' @param r_space [p, 3] rotated manipulation space 
+#' @param manip character string of the type of manipulation to use. Defaults to "radial". Alternatively use "horizontal" or "vertical" (fixed theta=0, pi/2 respectively)
+#' @param basis the [p, 2] orthonormal starting basis. Default to the identity basis 
+#' @param manip_var integer column or string name of the variable to manipulate. Required, does not default
+#' @param theta the angle that the manipvar should be rotated by in parameter space
+#' @param from value of phi to start the projection. Defaults to 0
+#' @param to value of phi to end the projection. Defaults to 0
+#' @param by the increment phi should change by between projections. Defaults to pi/20
 #' @export
 #' @examples
 #' data <- flea[, 1:6]
 #' p <- ncol(data)
-#' basis <- basis_random(p = p)
-#' m_sp <- create_manip_space(basis = b, manip_var = 3)
+#' b_rand <- basis_random(p = p)
 #' 
-#' data_proj(data=flea[1:3, 1:6], manip="radial", to=2)
-#' dp <- data_proj(data=data,  manip_var=3, manip="radial",
-#'                 from=0, to=pi, by=pi/10,
-#'                 manip_space = m_sp, theta = pi/4)
+#' data_proj(data=data[1:3, ], manip_var=1, manip="horizontal", to=pi/10)
+#' 
+#' dp <- data_proj(data=data, basis=b_rand, manip_var=3, manip="radial",
+#'                 from=0, to=pi, by=pi/10, theta=pi/4)
 #' str(dp)
 #' 
-data_proj <- function(data, manip="radial", manip_var = 1,
-                manip_space = create_manip_space(basis = basis_random(p = p),
-                                                 manip_var = manip_var), 
-                theta = 0, #phi controled by to, from, by.
-                from = 0, to = 0, by = pi / 20) {
+data_proj <- function(data, manip = "radial", basis = basis_identity(p = ncol(data)),
+                      manip_var, theta = 0, center=T, from = 0, to = 0, by = pi/20 ) {
   ### redundant explicit try catch
-  #tc_seq <- tryCatch(seq(from, to, by)  
+  #tc_seq <- tryCatch(seq(from, to, by)
   #                   ,error=function(e) e, warning=function(w) w)
   #if (any(class(tc_seq) == "error")) stop(tc_seq)
-  if (is.character(manip_var)) manip_var <- match("manip_var",names(data))
+  if (is.character(manip_var)) manip_var <- match(manip_var, names(data))
   if (!is.matrix(data)) data <- as.matrix(data)
   
   index <- 0
   projected_data <- NULL
   manip <- tolower(substr(manip, 1, 3))
+  manip_space = create_manip_space(basis = basis, manip_var = manip_var)
   
   for (phi in seq(from, to, by)) {
     index <- index+1
     if (manip == "rad") {
-      delta <- cbind(data %*% radial_manip(manip_space, phi, theta), index, phi)
+      delta <- cbind(data %*% radial_manip(manip_space, phi, theta), 
+                     index, manip_var, phi, theta)
     }
     else if (manip == "hor") {
-      delta <- cbind(data %*% horizontal_manip(manip_space, phi, theta), index, phi)
+      delta <- cbind(data %*% horizontal_manip(manip_space, phi, theta), 
+                     index, manip_var, phi, theta)
     }
     else if (manip == "ver") {
-      delta <- cbind(data %*% vertical_manip(manip_space, phi, theta), index, phi)
+      delta <- cbind(data %*% vertical_manip(manip_space, phi, theta), 
+                     index, manip_var, phi, theta)
     }
     projected_data <- rbind(projected_data, delta)
   }
@@ -64,24 +70,25 @@ data_proj <- function(data, manip="radial", manip_var = 1,
 #'
 #' @param projected_data the output of data_proj(), a series data projections being rotated.
 #' @param delay the delay (in seconds) between projections. Defaults to .5
+#' @param ggplot TRUE plots with ggplot2::ggplot(), FALSE plots with plot(). Defaults to TRUE
 #' @param from index to start the slideshow at. Defaults to 1
-#' @param to index to start the slideshow at. Defaults to max(index)
-#' @param by index to start the slideshow at. Defaults to 1
+#' @param to index to end the slideshow at. Defaults to max(index)
+#' @param by number of indexs to increment by between plots. Defaults to 1
 #' @import ggplot2
 #' @export
 #' @examples
 #' data <- flea[, 1:6]
 #' p <- ncol(data)
 #' basis <- basis_random(p = p)
-#' m_sp <- create_manip_space(basis = b, manip_var = 3)
 #' 
-#' dp <- data_proj(data=data,  manip_var=3, manip="radial",
-#'                 from=0, to=pi, by=pi/10,
-#'                 manip_space = m_sp, theta = pi/4)
+#' dp <- data_proj(data=data, basis=b_rand, manip_var=3, manip="radial",
+#'                from=0, to=pi, by=pi/10, theta=pi/4)
 #' slideshow(dp, ggplot=T)
+#' slideshow(dp, ggplot=F, from=2, to=8, by=2, delay=2)
 
 slideshow <- function(projected_data, delay=.5, ggplot = T,
-                      from = 1, to = max(projected_data[, 4]), by = 1) {
+                      from = 1, to = max(projected_data[, 4]), by = 1,
+                      col = "black", pch = 16) {
   xmin <- min(projected_data[, 1])
   xmax <- max(projected_data[, 1])
   ymin <- min(projected_data[, 2])
@@ -90,15 +97,16 @@ slideshow <- function(projected_data, delay=.5, ggplot = T,
   
   for (i in seq(from, to, by)) {
     d <- as.data.frame(projected_data[which(projected_data[, 4] == i), ]) 
-    t <- paste("Projected data. index=",i,"/",imax,". phi=",round(phi,2))
+    t <- paste("Projected data. index=",i,"of",imax,". phi=",round(d$phi[1],2))
     if (ggplot == T) {
-      print( ggplot2::ggplot(d, aes(x = d$x, y = d$y)) + geom_point() +
-        ggtitle(t) + ylab("") + xlab("") + 
-        xlim(c(xmin, xmax)) + ylim(c(ymin, ymax)) )
+      print( ggplot2::ggplot(d, ggplot2::aes(x = d$x, y = d$y)) + 
+               ggplot2::geom_point(color=col) + ggplot2::ggtitle(t) + 
+               ggplot2::ylab("") + ggplot2::xlab("") + 
+               ggplot2::xlim(c(xmin, xmax)) + ggplot2::ylim(c(ymin, ymax)) )
     } 
     else {
-      print( plot(x = d$x, y = d$y, main = t, ylab = "", xlab = "",
-                  xlim=c(xmin, xmax), ylim=c(ymin, ymax)) )
+      plot(x = d$x, y = d$y, main = t, ylab = "", xlab = "",
+          xlim=c(xmin, xmax), ylim=c(ymin, ymax), col=col, pch=pch)
     }
     Sys.sleep(delay)
   }
--- scratchpad.R
+++ scratchpad.R
@@ -12,16 +12,15 @@ library(spinifex)
 ### init
 data <- flea[, 1:6]
 p <- ncol(data)
-basis <- basis_random(p = p)
-m_sp <- create_manip_space(basis = b, manip_var = 3)
+b_rand <- basis_random(p = p)
 
 ### data_proj
-data_proj(data=flea[1:3, 1:6], manip="radial", to=2)
-dp <- data_proj(data=data,  manip_var=3, manip="radial",
-          from=0, to=pi, by=pi/10,
-          manip_space = m_sp, theta = pi/4)
+data_proj(data=data[1:3, 1:6], manip_var=1,  manip="radial", to=pi/10)
+dp <- data_proj(data=data, basis=b_rand, manip_var=3, manip="radial",
+          from=0, to=pi, by=pi/10, theta=pi/4)
+str(dp)
 slideshow(dp, ggplot=T)
-slideshow(dp, ggplot=F)
+slideshow(dp, ggplot=F, from=2, to=8, by=2, delay=2)
 
 ### manipulations
 horizontal_manip(manip_space = m_sp, phi = pi/3, theta = pi/4)
@@ -39,31 +38,27 @@ stop()
 ##### Vignette example 1: flea
 data <- flea[, 1:6]
 p <- ncol(data)
-basis <- basis_random(p = p)
-m_sp <- create_manip_space(basis = b, manip_var = 3)
+b_rand <- basis_random(p = p)
 
-data_proj(data=flea[1:3, 1:6], manip="radial", to=2)
-dp <- data_proj(data=data,  manip_var=3, manip="radial",
-                from=0, to=pi, by=pi/10,
-                manip_space = m_sp, theta = pi/4)
+dp <- data_proj(data=data, basis=b_rand, manip_var=3, manip="radial",
+                from=0, to=pi, by=pi/10, theta = pi/4)
 slideshow(dp, ggplot=T)
-slideshow(dp, ggplot=F)
+slideshow(dp, ggplot=F, from=2, to=8, by=2, delay=2)
 
 ##### Vignette example 2: nasa
-
-data <- nasa[
+nasa <- nasa[
   nasa$date >= as.POSIXct("1998-01-01") &
   nasa$lat >= 20 &
   nasa$lat <= 40 &
   nasa$long >= -80 &
   nasa$long <= -60
   , ]
-p <- ncol(data)
 str(nasa) #1:6 demographic, 7:13 var, 14:17 demographic,
+dim(table(nasa$day)) #grain: month. 72 months, (6 years)
 head(nasa[,7:13]) #12 is surftemp
 
 #?GGally::glyphs #see example
-temp.gly <- glyphs(data, "long", "day", "lat", "surftemp", height=2.5)
+temp.gly <- glyphs(nasa, "long", "day", "lat", "surftemp", height=2.5)
 str(temp.gly)
 ggplot2::ggplot(temp.gly, ggplot2::aes(gx, gy, group = gid)) +
   add_ref_lines(temp.gly, color = "grey90") +
@@ -72,4 +67,41 @@ ggplot2::ggplot(temp.gly, ggplot2::aes(gx, gy, group = gid)) +
   ggplot2::theme_bw() +
   ggplot2::labs(x = "", y = "")
 
+data <- nasa[,7:13]
+p <- ncol(nasa)
+
+##### Vignette example 3: guided tour on flea
+library(tourr)
+data <- flea[, 1:6]
+p <- ncol(data)
+#holes is unsupervised.
+holes <- tourr::save_history(data, tourr::guided_tour(index = holes), max_bases = 25)
+holes_basis <- matrix(as.numeric(holes[,,dim(holes)[3]]),ncol=2)
+#lda is supervised.
+lda <- tourr::save_history(data, tourr::guided_tour(
+  index = tourr::lda_pp(flea$species)),max_bases = 25)
+lda_basis <- matrix(as.numeric(lda[,,dim(lda)[3]]),ncol=2)
+#holes_basis, lda_basis
+
+
+str(data)
+GGally::ggpairs(data)
+#head, aede1, tars1?
+#aede1 shows promise theta=90, phi close to 90
+head <- data_proj(data=data, basis=holes_basis, manip_var="head", manip="radial",
+                   from=0, to=pi, by=pi/10, theta = 0)
+aede2 <- data_proj(data=data, basis=holes_basis, manip_var="aede1", manip="radial",
+                   from=0, to=pi, by=pi/10, theta = 0)
+tars2 <- data_proj(data=data, basis=holes_basis, manip_var="tars1", manip="radial",
+                from=0, to=pi, by=pi/10, theta = 0)
+slideshow(head)
+slideshow(aede2)
+slideshow(tars2)
+
+off <- data_proj(data=data, basis=holes_basis, manip_var="aede3", manip="radial",
+                   from=0, to=pi, by=pi/10, theta = pi/5)
+slideshow(off)
+#aede2 and head do well in distinguishing species!
+#aede1, aede3, and tars1 do ok in distinguishing species.
+#tars2 does a poor job in distinguishing species.
 
