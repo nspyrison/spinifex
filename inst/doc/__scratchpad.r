7e71d5c9c64a24a76472cee00c6f68a72b12f059
Nicholas Spyrison <spyrison@gmail.com>
2018-10-29 14:28:27 +1100

    inst/doc recover

---
+++ inst/doc/__scratchpad.r
@@ -0,0 +1,27 @@
+### Testing and scratchpad:
+devtools::document()
+devtools::build()
+
+devtools::load_all()
+devtools::install()
+
+
+### Phys Example
+load("data/PhysDat_6PC.rda")
+load("data/fGT_6d.rda")
+phys.end <- matrix(as.numeric(fGT_6d[,,dim(fGT_6d)[3]]),ncol=2)
+
+col <- PhysDat$disID
+pch <- PhysDat$disID
+
+proj <-
+  proj_data(
+    data = PhysDat[, 1:6],
+    basis = phys.end,
+    manip_var = 1, 
+    manip_type = "rad",
+    phi_from = 0,
+    phi_to = 2*pi,
+    n_slides = 20
+  )
+slideshow(proj,col = col, pch = pch)
---
+++ inst/doc/_attr scratchpad.r
@@ -0,0 +1,40 @@
+# create a 2 by 5 matrix
+x <- 1:10
+attr(x,"MyAttr") <- c(2, 5, "foo")
+dim(x) # vectors don't have dim.
+str(x)
+attributes(x)
+class(x) # integer vector
+str(x[1]) # changing or referencing easily breaks attributes.
+
+attr(x, exact = "class")
+
+food = as.data.frame(c(apple=2, grape=3))
+attr(food[, ], "foodType") <- "fruit"
+food
+
+mt <- tibble::as.tibble(mtcars)
+mt
+
+attr(mt$cyl, "has6Cyl") <- ifelse(mt$cyl==6, TRUE, FALSE) # can set column attrs
+mt
+mt$cyl
+str(mt)
+str(mt$cyl)
+attributes(mt)
+attributes(mt$cyl)
+attributes(str(mt))
+attributes(str(mt$cyl))
+
+ray <- array(data = 1:12, dim = c(2,2,3))
+dim(ray)
+str(ray)
+attributes(ray) # just dim
+class(ray) # array
+str(ray[,1,1]) # changing or referencing easily breaks attributes
+
+attr(ray, "data_CrDate") <- as.Date("2018-10-03")
+attr(ray, "page") <- c(rep(1, 4), rep(2, 4), rep(3, 4))
+dim(ray)
+str(ray)
+str(ray[,1,1]) # changing or referencing easily breaks attributes
