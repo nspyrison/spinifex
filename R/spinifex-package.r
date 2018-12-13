ad64e1887940178c6e118831e01524d2c4c43136
Nicholas Spyrison <spyrison@gmail.com>
2018-12-13 13:51:12 +1100

    build ignore still not working. follow up with njt about build ignore and ... error not fining manip_type

---
+++ R/spinifex-package.r
@@ -0,0 +1,32 @@
+#' spinifex
+#'
+#' spinifex is a package that allows for manual tours. Touring is a linear 
+#' dimensionality reducetion method that projects objects from `p` down to `d` 
+#' dimensions. Manual tours manipulate one selected variable by a given ammount.
+#' spinifex can also consume tour paths generated in the tourr package and
+#' offers streamlined production of plotly graphics to animate the tour.
+#'
+#' It's main functions are:
+#' \itemize{
+#'   \item [manual_tour()]
+#'   \item [create_slides()]
+#'   \item [render_slideshow()]
+#'   \item [spinifex()], A wrapper fucntion for the above.
+#'   \item [play_tour()], A wrapper function which tourr tour paths can be used.
+#' }
+#'
+#' GitHub repo: \url{https://github.com/nspyrison/spinifex}
+#' @seealso tourr (package)
+#' @name spinifex
+#' @docType package
+"_PACKAGE"
+
+globalVariables(c("cat_var",
+                  "manip_type",
+                  "theta",
+                  "phi_min",
+                  "phi_max",
+                  "n_slides",
+                  "disp_type",
+                  "manip_col"
+))
