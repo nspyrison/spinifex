4bf72dfad78d1ff74b02e66f0cb3da9e96982b72
Nicholas Spyrison <spyrison@gmail.com>
2019-01-20 22:01:31 +1100

    view_basis to ggplot grapics

--- R/util.r
+++ R/util.r
@@ -16,15 +16,36 @@
 #' rb <- tourr::basis_random(4, 2)
 #' view_basis(basis = rb)
 #' @export
-view_basis <- function(basis = tourr::basis_init(6,2), 
-                       labels = paste0("V", 1:nrow(basis)), 
-                       ...) {
-  graphics::plot(0, asp = 1, type = 'n', axes = FALSE, ann = FALSE,
-                 xlim = c(-1, 1), ylim = c(-1, 1))
-  graphics::segments(0, 0, basis[, 1], basis[, 2], ...)
-  theta <- seq(0, 2 * pi, length = 360)
-  graphics::lines(cos(theta), sin(theta), ...)
-  graphics::text(basis[, 1], basis[, 2], label = labels, ...)
+view_basis <- funtion(basis,
+                      labels = paste0("V", 1:nrow(basis)) ) {
+  # Initialize
+  p <- nrow(basis)
+  basis <- as.data.frame(basis)
+  colnames(basis) <- paste0("C", 1:ncol(basis))
+  ## circle
+  angle <- seq(0, 2 * pi, length = 360)
+  circ  <- data.frame(x = cos(angle), y = sin(angle))
   
-  return(basis)
+  # graphics (reference frame)
+  ## circle and set options
+  gg1 <- ggplot2::ggplot() + ggplot2::geom_path(
+    data = circ, color = "grey80", size = .3, inherit.aes = F,
+    mapping = ggplot2::aes(x = circ$x, y = circ$y)
+  ) +
+    ggplot2::scale_color_brewer(palette = "Dark2") +
+    ggplot2::theme_void() +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::coord_fixed() # Do not use with plotly!
+  ## Axes line segments
+  gg2 <- gg1 +
+    ggplot2::geom_segment(
+      data = basis, mapping = ggplot2::aes(x = C1, y = C2, xend = 0, yend = 0)
+    )
+  ## Text labels
+  gg3 <- gg2 + ggplot2::geom_text(
+    data = basis, size = 4, hjust = 0, vjust = 0, colour = "black",
+    mapping = ggplot2::aes(x = C1, y = C2, label = labels)
+  )
+  
+  gg3
 }
