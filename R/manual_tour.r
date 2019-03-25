90f35981f275f320f937c17fc99ea131facdbc91
Nicholas Spyrison <spyrison@gmail.com>
2019-03-26 10:43:25 +1100

    phi transform fixed

--- R/manual_tour.r
+++ R/manual_tour.r
@@ -121,8 +121,8 @@ manual_tour <- function(basis = NULL,
   # Find phi's path
   find_path <- function(start, end)
   {
-    start <- pi/2 - (start - phi_start)
-    end   <- pi/2 - (end - phi_start)
+    start <- -1 * (start - phi_start)
+    end   <- -1 * (end - phi_start)
     dist  <- abs(end - start)
     steps <- round(dist/angle)
     angle <- dist/steps
