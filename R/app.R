817d8a45cb66b81f25d4bb18c822d05b2fa46445
Nicholas Spyrison <spyrison@gmail.com>
2019-04-30 14:10:51 +1000

    WIP: adding canned datasets

--- R/app.R
+++ R/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(plotly)
 library(spinifex)
+library(dplyr)
 # library(eechidna); eechidna::launch_app()
 
 f_dat  <- tourr::rescale(flea[,1:6])
@@ -9,14 +10,32 @@ f_cat  <- factor(flea$species)
 f_path <- save_history(f_dat, guided_tour(holes()))
 f_bas  <- matrix(f_path[,, max(dim(f_path)[3])], ncol=2)
 
+olive %>% 
+  count(paste(olive$area, olive$palmitic), sort=T)
+  
+?olive
+head(olive)
+o_dat <- olive[, 3:10]
+o_cat <- olive$area
+
 
 ui <- fluidPage(
   headerPanel("Spinifex app - radial manual tour"),
+  
   sidebarPanel(
+    radioButtons("radio_data", label = h2("Radio buttons"),
+                 choices = list("flea" = 1, "olive" = 2), 
+                 selected = 1),
+  
+      verbatimTextOutput("data_value"),
+    
     # selectInput('guide', 'Guided tour basis', 
     #             c('holes()','cmass()','lda_pp()'), 'holes()', multiple = FALSE),
+    
     selectInput('mvar', 'Manip var', colnames(f_dat), selected = "aede2"),
+    
     checkboxInput("show", "More options"),
+    
     conditionalPanel("input.show",
                      selectInput('axes', 'Reference axes', 
                                  c('center','bottomleft','off'), 'center', multiple = FALSE),
@@ -30,7 +49,13 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-
+  
+  output$data_value <- renderPrint({ #input$radio_data 
+    if (input$radio_data == 1) {"flea dataset"
+    } else if (input$radio_data == 2) {"olive dataset"
+    } else {"data not found"}
+  })
+  
   output$plotlytest <- renderPlotly({
     play_manual_tour(data = f_dat, basis = f_bas, 
                      manip_var = which( colnames(f_dat)==input$mvar ),
@@ -43,3 +68,4 @@ server <- function(input, output) {
 }
 
 shinyApp(ui = ui, server = server)
+
