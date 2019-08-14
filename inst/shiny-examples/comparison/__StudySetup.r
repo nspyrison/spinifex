b65436e3e5d419500d8f2c1020daf02b8e39910e
Nicholas Spyrison <spyrison@gmail.com>
2019-08-14 18:41:35 +1000

    file cleanup

---
+++ inst/shiny-examples/comparison/__StudySetup.r
@@ -0,0 +1,17 @@
+### looking for data sets:
+# https://archive.ics.uci.edu/ml/datasets.php?format=&task=&att=&area=&numAtt=&numIns=&type=&sort=dateDown&view=table
+# https://en.wikipedia.org/wiki/List_of_datasets_for_machine-learning_research#Anomaly_data
+
+
+### ended with: (some files needed file conversions)
+# https://archive.ics.uci.edu/ml/datasets/Caesarian+Section+Classification+Dataset
+# https://archive.ics.uci.edu/ml/datasets/BuddyMove+Data+Set
+# https://archive.ics.uci.edu/ml/datasets/Somerville+Happiness+Survey
+# https://archive.ics.uci.edu/ml/datasets/Absenteeism+at+work
+# https://archive.ics.uci.edu/ml/datasets/seeds
+# https://archive.ics.uci.edu/ml/datasets/Real+estate+valuation+data+set
+
+### Also see link from Di: 
+# https://www.verywellmind.com/how-to-conduct-a-psychology-experiment-2795792
+library(spinifex)
+run_app("comparison")
