02f13d0381650078f40085b8bab93f4d1d6d4077
Nicholas Spyrison <spyrison@gmail.com>
2020-12-14 15:31:43 +1100

    stepwise progress, but not zeroing correctly when exactly one projection contribution is negative.

--- R/b1_stepwise_tour.r
+++ R/b1_stepwise_tour.r
@@ -1,3 +1,9 @@
+#' @param start_basis A full (p x 2) basis to start removing variables from.
+#' @param data The data to project.
+#' @param measure A univariate function to apply to each of the variables of 
+#' the data.
+#' @param curr_dim 
+#' 
 #' @examples
 #' dat <- wine[, 2:14]
 #' bas <- basis_pca(dat)
@@ -8,58 +14,57 @@
 
 if(F){
   start_basis = bas; data = dat;
-  measure = var; curr_dim = ncol(data); decreasing = TRUE
+  measure = var; curr_dim = ncol(data); decreasing = TRUE;
 }
-stepwise_hist <- function(start_basis = NULL, 
-                          data = NULL, 
+stepwise_hist <- function(start_basis = NULL,
+                          data = NULL,
                           measure = var,
                           curr_dim = ncol(data), ##needed or no? maybe a start dim?
-                          decreasing = TRUE
-){
-  #measure <- match.arg(measure)
+                          decreasing = TRUE){
   if(is.null(start_basis) == TRUE){start_basis <- tourr::basis_random(curr_dim, 2)}
   
   ## Initialize
   data <- as.matrix(data)
   p <- ncol(data)
-  if(all(dim(start_basis) != c(p, 2)) == TRUE) 
+  if(all(dim(start_basis) != c(p, 2)) == TRUE)
     stop("Dimension of the starting basis not [p x 2].")
   
   ## Measure table, ordered
-  m <- apply(data, 2, measure)
+  m   <- apply(data, 2, measure)
   ord <- order(m, decreasing = decreasing)
-  m <- m[ord]
-  cn <- colnames(data)[ord]
-  measure_tbl <- 
+  m   <- m[ord]
+  cn  <- colnames(data)[ord]
+  ## Reorder basis, so we can operate on the last var without issue.
+  basis_ord <- start_basis[ord, ]
+  measure_tbl <-
     data.frame(var_nm = factor(cn, levels = cn),
-               var_ord = ord, 
+               var_ord = ord,
                var_measure = m,
                cumsum_measure = cumsum(m) / sum(m)
-  )
+    )
   
   ## The work, making the basis list
   basis_ls <- list(NULL)
-  basis_ls[[p]] <- start_basis
+  basis_ls[[p]] <- basis_ord
   sapply((p - 1L):2L, function(i){ ## i is the dim of the new_bas, this_* operates on i + 1
-    #for(i in p-1L:2L){
+  #for(i in (p - 1L):2L){
     this_basis <- basis_ls[[i + 1L]]
-    this_mv    <- measure_tbl$var_ord[i + 1L] 
+    this_mv <- i + 1L
     ##TODO THE LINE ABOVE IS GOING TO BE OFF AT SOME PT BECUSE WE ARE CHANGING THE DIM FO THE BASIS, ALSO NEED TO UPDATE THE MEASURE_TBL.
-    this_theta <- atan(this_basis[this_mv, 2L] / this_basis[this_mv, 1L])
-    
+    this_theta <- atan(this_basis[this_mv, 2L] / this_basis[this_mv, 1L]) ## Radial
+    this_phi   <- .5 * pi + acos(sqrt(this_basis[this_mv, 1L]^2L + this_basis[this_mv, 2L]^2L)) ## Negative of the current phi
     this_m_sp  <- create_manip_space(basis = this_basis, manip_var = this_mv)
     new_bas    <- rotate_manip_space(manip_space = this_m_sp,
-                                     theta = this_theta, phi = .5* pi)[-this_mv, 1L:2L]
+                                     theta = this_theta, phi = this_phi)[, 1L:2L]
+    is_orthonormal(new_bas)
     
-    basis_ls[[i]] <<- new_bas
-    #}
-  })
+    basis_ls[[i]] <<- new_bas ## Add next lvl assignment when vectorizing
+  }) ##TODO, WHEN THERE IS EXCLUSIVELY 1 NEG PROJ COMPONENT IS DOESN"T GET ZEROED CORRECTLY.
   
   ret <- list(data = data,
               measure_tbl = measure_tbl,
               basis_ls = basis_ls,
-              curr_dim = curr_dim
-  )
+              curr_dim = curr_dim)
   return(ret)
 }
 
