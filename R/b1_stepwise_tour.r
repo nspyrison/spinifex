fe492291a4af9f6649a52f244aff1d89d7ddeee9
Nick Spyrison <spyrison@gmail.com>
2020-12-23 14:56:22 +1100

    rounding, clean up, setup for TS zeroing.

--- R/b1_stepwise_tour.r
+++ R/b1_stepwise_tour.r
@@ -1,4 +1,4 @@
-#' @param start_basis A full (p x 2) basis to start removing variables from.
+#' @param basis A full (p x 2) basis to start removing variables from.
 #' @param data The data to project.
 #' @param measure A univariate function to apply to each of the variables of 
 #' the data.
@@ -8,25 +8,25 @@
 #' dat <- wine[, 2:14]
 #' bas <- basis_pca(dat)
 #' 
-#' sw_ret <- stepwise_hist(start_basis = bas, data = dat)
+#' sw_ret <- stepwise_hist(basis = bas, data = dat)
 #' 
 #' 
 
 if(F){
-  library(spinifex); data = wine[, 2:14]; start_basis = basis_pca(data);
+  library(spinifex); data = wine[, 2:14]; basis = basis_pca(data);
   measure = var; curr_dim = ncol(data); decreasing = TRUE;
 }
-stepwise_hist <- function(start_basis = NULL,
+stepwise_hist <- function(basis = NULL,
                           data = NULL,
                           measure = var,
                           curr_dim = ncol(data), ##needed or no? maybe a start dim?
                           decreasing = TRUE){
-  if(is.null(start_basis) == TRUE){start_basis <- tourr::basis_random(curr_dim, 2)}
+  if(is.null(basis) == TRUE){basis <- tourr::basis_random(curr_dim, 2)}
   
   ## Initialize
   data <- as.matrix(data)
   p <- ncol(data)
-  if(all(dim(start_basis) != c(p, 2)) == TRUE)
+  if(all(dim(basis) != c(p, 2)) == TRUE)
     stop("Dimension of the starting basis not [p x 2].")
   
   ## Measure table, ordered
@@ -35,7 +35,7 @@ stepwise_hist <- function(start_basis = NULL,
   m   <- m[ord]
   cn  <- colnames(data)[ord]
   ## Reorder basis, so we can operate on the last var without issue.
-  basis_ord <- start_basis[ord, ]
+  basis_ord <- basis[ord, ]
   measure_tbl <-
     data.frame(var_nm = factor(cn, levels = cn),
                var_ord = ord,
@@ -46,7 +46,7 @@ stepwise_hist <- function(start_basis = NULL,
   ## The work, making the basis list
   basis_ls <- list(NULL)
   basis_ls[[p]] <- basis_ord
-  mute_apply <- sapply((p - 1L):2L, function(i){ ## i is the dim of the new_bas, this_* operates on i + 1
+  mute_apply <- sapply((p - 1L):1L, function(i){ ## i is the dim of the new_bas, this_* operates on i + 1
   #for(i in (p - 1L):2L){
     this_basis <- basis_ls[[i + 1L]]
     this_mv <- i + 1L
@@ -56,9 +56,8 @@ stepwise_hist <- function(start_basis = NULL,
     this_m_sp  <- create_manip_space(basis = this_basis, manip_var = this_mv)
     new_bas    <- rotate_manip_space(manip_space = this_m_sp,
                                      theta = this_theta, phi = this_phi)[, 1L:2L]
-    is_orthonormal(new_bas)
     
-    basis_ls[[i]] <<- new_bas ## Add next lvl assignment when vectorizing
+    basis_ls[[i]] <<- round(new_bas, 5) ## Add next lvl assignment when vectorizing
   }) ##TODO, WHEN THERE IS EXCLUSIVELY 1 NEG PROJ COMPONENT IS DOESN"T GET ZEROED CORRECTLY.
   
   ret <- list(data = data,
@@ -68,3 +67,17 @@ stepwise_hist <- function(start_basis = NULL,
   return(ret)
 }
 
+
+
+if(F){ ### MANUAL TESTING ------
+  library(spinifex); library(spinifex); 
+  dat = tourr::flea[, 2:6];
+  bas = basis_pca(dat);
+  stepwise_hist(basis = bas, data = dat)
+  
+  ## aede2(+/+) zeros, 
+  ## head(-/+) doesn't zero (which then causes an issue)
+  ## tars2(-/+ !?!) zeros, why? may not be as simple as quadrant.
+  
+  ##TODO need to look at phi and theta 
+} 
